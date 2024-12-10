0865ee26798310154a6e408e0bbc8b3b1a10082a
Илья Рукавишников <ruk@mgpu.ru>
2024-12-10 14:07:41 +0300

    Updated config.php

--- config.php
+++ config.php
@@ -3,7 +3,7 @@
 $engine_folder = __DIR__ . DIRECTORY_SEPARATOR . 'engines';
 $engine_extension = '.exe';
 
-$engine_default = 'RukChess 3.0.18 NNUE2';
+$engine_default = 'RukChess 4.0.0 NNUE2';
 $fen_default = 'rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1'; // Start position
 $depth_default = 0; // None
 $move_time_default = 3000; // 3 sec.
